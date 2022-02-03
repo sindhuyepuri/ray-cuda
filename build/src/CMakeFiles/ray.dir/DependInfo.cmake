eddf69363ac8d6e06ce8495c8fa2369c577c3713
Sindhu Yepuri <sindhu@Sindhus-MBP-2.grandenetworks.net>
2022-02-03 00:57:29 -0600

    initial commit

---
+++ build/src/CMakeFiles/ray.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/sindhu/Downloads/ray/src/RayTracer.cpp" "src/CMakeFiles/ray.dir/RayTracer.cpp.o" "gcc" "src/CMakeFiles/ray.dir/RayTracer.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/SceneObjects/Box.cpp" "src/CMakeFiles/ray.dir/SceneObjects/Box.cpp.o" "gcc" "src/CMakeFiles/ray.dir/SceneObjects/Box.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/SceneObjects/Cone.cpp" "src/CMakeFiles/ray.dir/SceneObjects/Cone.cpp.o" "gcc" "src/CMakeFiles/ray.dir/SceneObjects/Cone.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/SceneObjects/Cylinder.cpp" "src/CMakeFiles/ray.dir/SceneObjects/Cylinder.cpp.o" "gcc" "src/CMakeFiles/ray.dir/SceneObjects/Cylinder.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/SceneObjects/Sphere.cpp" "src/CMakeFiles/ray.dir/SceneObjects/Sphere.cpp.o" "gcc" "src/CMakeFiles/ray.dir/SceneObjects/Sphere.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/SceneObjects/Square.cpp" "src/CMakeFiles/ray.dir/SceneObjects/Square.cpp.o" "gcc" "src/CMakeFiles/ray.dir/SceneObjects/Square.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/SceneObjects/trimesh.cpp" "src/CMakeFiles/ray.dir/SceneObjects/trimesh.cpp.o" "gcc" "src/CMakeFiles/ray.dir/SceneObjects/trimesh.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/fileio/bitmap.cpp" "src/CMakeFiles/ray.dir/fileio/bitmap.cpp.o" "gcc" "src/CMakeFiles/ray.dir/fileio/bitmap.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/fileio/buffer.cpp" "src/CMakeFiles/ray.dir/fileio/buffer.cpp.o" "gcc" "src/CMakeFiles/ray.dir/fileio/buffer.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/fileio/images.cc" "src/CMakeFiles/ray.dir/fileio/images.cc.o" "gcc" "src/CMakeFiles/ray.dir/fileio/images.cc.o.d"
+  "/Users/sindhu/Downloads/ray/src/fileio/pngimage.cpp" "src/CMakeFiles/ray.dir/fileio/pngimage.cpp.o" "gcc" "src/CMakeFiles/ray.dir/fileio/pngimage.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/main.cpp" "src/CMakeFiles/ray.dir/main.cpp.o" "gcc" "src/CMakeFiles/ray.dir/main.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/parser/Parser.cpp" "src/CMakeFiles/ray.dir/parser/Parser.cpp.o" "gcc" "src/CMakeFiles/ray.dir/parser/Parser.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/parser/ParserException.cpp" "src/CMakeFiles/ray.dir/parser/ParserException.cpp.o" "gcc" "src/CMakeFiles/ray.dir/parser/ParserException.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/parser/Token.cpp" "src/CMakeFiles/ray.dir/parser/Token.cpp.o" "gcc" "src/CMakeFiles/ray.dir/parser/Token.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/parser/Tokenizer.cpp" "src/CMakeFiles/ray.dir/parser/Tokenizer.cpp.o" "gcc" "src/CMakeFiles/ray.dir/parser/Tokenizer.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/scene/bbox.cc" "src/CMakeFiles/ray.dir/scene/bbox.cc.o" "gcc" "src/CMakeFiles/ray.dir/scene/bbox.cc.o.d"
+  "/Users/sindhu/Downloads/ray/src/scene/camera.cpp" "src/CMakeFiles/ray.dir/scene/camera.cpp.o" "gcc" "src/CMakeFiles/ray.dir/scene/camera.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/scene/cubeMap.cpp" "src/CMakeFiles/ray.dir/scene/cubeMap.cpp.o" "gcc" "src/CMakeFiles/ray.dir/scene/cubeMap.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/scene/light.cpp" "src/CMakeFiles/ray.dir/scene/light.cpp.o" "gcc" "src/CMakeFiles/ray.dir/scene/light.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/scene/material.cpp" "src/CMakeFiles/ray.dir/scene/material.cpp.o" "gcc" "src/CMakeFiles/ray.dir/scene/material.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/scene/ray.cpp" "src/CMakeFiles/ray.dir/scene/ray.cpp.o" "gcc" "src/CMakeFiles/ray.dir/scene/ray.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/scene/scene.cpp" "src/CMakeFiles/ray.dir/scene/scene.cpp.o" "gcc" "src/CMakeFiles/ray.dir/scene/scene.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/ui/CommandLineUI.cpp" "src/CMakeFiles/ray.dir/ui/CommandLineUI.cpp.o" "gcc" "src/CMakeFiles/ray.dir/ui/CommandLineUI.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/ui/CubeMapChooser.cxx" "src/CMakeFiles/ray.dir/ui/CubeMapChooser.cxx.o" "gcc" "src/CMakeFiles/ray.dir/ui/CubeMapChooser.cxx.o.d"
+  "/Users/sindhu/Downloads/ray/src/ui/GraphicalUI.cpp" "src/CMakeFiles/ray.dir/ui/GraphicalUI.cpp.o" "gcc" "src/CMakeFiles/ray.dir/ui/GraphicalUI.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/ui/ModelerCamera.cpp" "src/CMakeFiles/ray.dir/ui/ModelerCamera.cpp.o" "gcc" "src/CMakeFiles/ray.dir/ui/ModelerCamera.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/ui/TraceGLWindow.cpp" "src/CMakeFiles/ray.dir/ui/TraceGLWindow.cpp.o" "gcc" "src/CMakeFiles/ray.dir/ui/TraceGLWindow.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/ui/TraceUI.cc" "src/CMakeFiles/ray.dir/ui/TraceUI.cc.o" "gcc" "src/CMakeFiles/ray.dir/ui/TraceUI.cc.o.d"
+  "/Users/sindhu/Downloads/ray/src/ui/debuggingView.cpp" "src/CMakeFiles/ray.dir/ui/debuggingView.cpp.o" "gcc" "src/CMakeFiles/ray.dir/ui/debuggingView.cpp.o.d"
+  "/Users/sindhu/Downloads/ray/src/ui/debuggingWindow.cxx" "src/CMakeFiles/ray.dir/ui/debuggingWindow.cxx.o" "gcc" "src/CMakeFiles/ray.dir/ui/debuggingWindow.cxx.o.d"
+  "/Users/sindhu/Downloads/ray/src/ui/glObjects.cpp" "src/CMakeFiles/ray.dir/ui/glObjects.cpp.o" "gcc" "src/CMakeFiles/ray.dir/ui/glObjects.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
